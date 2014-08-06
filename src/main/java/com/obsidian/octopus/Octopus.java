6ca7b9697f4996a02668a01d282c1ad60f8f8078
alex.chou <alex.chou@obsidian-interactive.com>
2014-08-07 01:18:29 +0800

    first commit

---
+++ src/main/java/com/obsidian/octopus/Octopus.java
@@ -0,0 +1,15 @@
+package com.obsidian.octopus;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ *
+ * @author Alex Chou <xi.zhou at obsidian>
+ */
+public class Octopus {
+
+    public static void main(String[] args) {
+        boolean allLowerCase = StringUtils.isAllLowerCase("aaa");
+        System.out.println("Hello World" + allLowerCase);
+    }
+}
