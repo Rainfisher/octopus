f82509cc20663ecf3b4a7751429dadb85781a37b
alex.chou <alex.chou@obsidian-interactive.com>
2016-06-28 11:21:34 +0800

    bugfix/interceptor ip filter

--- src/main/java/com/obsidian/octopus/vulcan/interceptor/InterceptorIpFilter.java
+++ src/main/java/com/obsidian/octopus/vulcan/interceptor/InterceptorIpFilter.java
@@ -4,6 +4,7 @@ import com.obsidian.octopus.utils.IpUtils;
 import com.obsidian.octopus.vulcan.codec.HttpResponseMessage;
 import com.obsidian.octopus.vulcan.object.ActionContext;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.inject.Singleton;
 
@@ -17,14 +18,30 @@ public class InterceptorIpFilter implements Interceptor {
     private final List<IpObject> list = new ArrayList<>();
 
     public void clear() {
-        list.clear();
+        synchronized (list) {
+            list.clear();
+        }
     }
 
     public void addIptables(long ipStart, long ipEnd) {
         IpObject object = new IpObject();
         object.ipStart = ipStart;
         object.ipEnd = ipEnd;
-        list.add(object);
+        synchronized (list) {
+            list.add(object);
+        }
+    }
+
+    public void deleteOne(long ipStart, long ipEnd) {
+        synchronized (list) {
+            for (Iterator<IpObject> iterator = list.iterator(); iterator.hasNext();) {
+                IpObject ipObject = iterator.next();
+                if (ipObject.ipStart == ipStart && ipObject.ipEnd == ipEnd) {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
     }
 
     @Override
@@ -32,15 +49,19 @@ public class InterceptorIpFilter implements Interceptor {
         String ip = IpUtils.getIpAddr();
         long ipNumber = IpUtils.ip2Long(ip);
         boolean ret = false;
-        for (IpObject ipObject : list) {
-            Long key = ipObject.ipStart;
-            Long value = ipObject.ipEnd;
-            if (ipNumber >= key && ipNumber <= value) {
-                ret = true;
-                break;
+        synchronized (list) {
+            for (IpObject ipObject : list) {
+                Long key = ipObject.ipStart;
+                Long value = ipObject.ipEnd;
+                if (ipNumber >= key && ipNumber <= value) {
+                    ret = true;
+                    break;
+                }
             }
         }
-        if (!ret) {
+        if (ret) {
+            invocation.invoke();
+        } else {
             ActionContext.set(ActionContext.HTTP_RESPONSE_CODE, HttpResponseMessage.HTTP_STATUS_FORBIDDEN);
         }
         return ret;
