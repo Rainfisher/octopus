5f4aeb0ed7bb56d704d6d1b11aa4c979814f5aba
alex.chou <alex.chou@obsidian-interactive.com>
2016-06-29 11:08:08 +0800

    feature/set Router impl RouterImpl

--- src/main/java/com/obsidian/octopus/vulcan/core/Router.java
+++ src/main/java/com/obsidian/octopus/vulcan/core/Router.java
@@ -1,9 +1,13 @@
 package com.obsidian.octopus.vulcan.core;
 
+import com.google.inject.ImplementedBy;
+import com.obsidian.octopus.vulcan.object.RouterImpl;
+
 /**
  *
  * @author alex
  */
+@ImplementedBy(RouterImpl.class)
 public interface Router {
 
     Action getAction(String context);
