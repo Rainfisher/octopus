f6b6f1af43a58cd47ecb463422bc422e28d5ca3d
alex.chou <alex.chou@obsidian-interactive.com>
2017-03-09 17:56:42 +0800

    feature/action invoker add global interceptor param

--- src/main/java/com/obsidian/octopus/vulcan/object/ActionInvokerImpl.java
+++ src/main/java/com/obsidian/octopus/vulcan/object/ActionInvokerImpl.java
@@ -2,24 +2,33 @@ package com.obsidian.octopus.vulcan.object;
 
 import com.obsidian.octopus.ioc.IocInstanceProvider;
 import com.obsidian.octopus.vulcan.interceptor.ActionInvocation;
+import com.obsidian.octopus.vulcan.interceptor.Interceptor;
 import com.obsidian.octopus.vulcan.interceptor.InterceptorGlobal;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 /**
  *
  * @author alex
  */
-@Singleton
 public class ActionInvokerImpl implements ActionInvoker {
 
     @Inject
     private IocInstanceProvider provider;
 
+    private Class<? extends Interceptor> globalClass = InterceptorGlobal.class;
+
+    public Class<? extends Interceptor> getGlobalClass() {
+        return globalClass;
+    }
+
+    public void setGlobalClass(Class<? extends Interceptor> globalClass) {
+        this.globalClass = globalClass;
+    }
+
     @Override
     public boolean execute() throws Exception {
         ActionInvocation actionInvocation = new ActionInvocation(provider);
-        actionInvocation.addInterceptor(InterceptorGlobal.class);
+        actionInvocation.addInterceptor(globalClass);
         return actionInvocation.invoke();
     }
 
